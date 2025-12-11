d5ebaaf41017bb7b793652642c60abb6d4d797e5
Shashank Shetty <T2.SHSHETTY@dm1.na.local>
2025-12-11 13:40:52 +0530

    updated codes are pushed again

--- tests/Demo.spec.js
+++ tests/Demo.spec.js
@@ -32,26 +32,32 @@ test('DemoBlaze test', async ({ page }) => {
   await page.locator("#login2").click();
   await page.locator("#loginusername").fill("RamShetty3");
   await page.locator("#loginpassword").fill("123456");
-
   await page.locator("button[onclick='logIn()']").click();
-  await page.waitForTimeout(3000);
-  await page.screenshot({ path: 'demo-screenshot.png', fullPage: true });
-
 
   await page.locator("#tbodyid#tbodyid .card img").first().click()
   await page.locator(".btn.btn-success.btn-lg").click()
  
 
   await page.locator("#cartur").click()
+  await page.waitForTimeout(2000);
+
+  await page.locator(".btn.btn-success").click();
+  await page.waitForTimeout(1000);
+  await page.locator("#name").fill("My MObile");
+  await page.locator("#country").fill("Argentina");
+  await page.locator("#city").fill("Rosoria");
+  await page.locator("#card").fill("AXIS-bank 420");
+  await page.locator("#month").fill("june");
+  await page.locator("#year").fill("1987");
+
+  await page.locator("button[onclick='purchaseOrder()']").click();
+  await page.screenshot({ path: 'purcase-screenshot.png', fullPage: true });
+
+  await page.locator(".confirm.btn.btn-lg.btn-primary").click()
   await page.waitForTimeout(3000);
-  await page.screenshot({ path: 'demo1-screenshot.png', fullPage: true });
+  await page.screenshot({ path: 'afterpurcase-screenshot.png', fullPage: true });
 
-  
-
-
-
-
-
+  await page.waitForTimeout(3000)
 
 
 
